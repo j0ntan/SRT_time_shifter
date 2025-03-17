2f3f13970c2f4168efee1731db7dcaf49aee03cf
Jonathan Gamboa <jgamboa0209@gmail.com>
2025-03-17 13:14:45 -0400

    Add `Timecode` files

---
+++ include/Timecode.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+/**
+ * @brief  @brief A timecode object that represents a time in hours, minutes,
+ * seconds, and millisecond
+ *
+ * @note This timecode follows the standard SubRip timecode format, as
+ * described in https://en.wikipedia.org/wiki/SubRip#Format. This defines the
+ * valid range for  each time component.
+ *
+ */
+class Timecode
+{
+};
