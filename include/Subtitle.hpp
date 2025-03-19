222a9488d20552b82ddf6058f0bd6bfeab1ef203
Jonathan Gamboa <jgamboa0209@gmail.com>
2025-03-19 00:19:53 -0400

    Add `Subtitle` files

---
+++ include/Subtitle.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+/**
+ * @brief A subtitle frame that follows the SubRip format
+ *
+ */
+class Subtitle
+{
+};
---
+++ src/Subtitle.test.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+
+#include <Subtitle.hpp>
+
+TEST(SubtitleCtor, createDefault)
+{
+    Subtitle subtitle;
+}
